dfc7dcbe8692054947353d11de1a21f8e380ac8c
Drew Miley <drewmiley92@gmail.com>
2017-07-30 23:13:03 +0100

    Now harnessed Fibonacci

--- static/components/Fibonacci.jsx
+++ static/components/Fibonacci.jsx
@@ -4,7 +4,7 @@ export default class Fibonacci extends React.Component {
 	constructor(props) {
 		super(props);
 		this.shouldComponentUpdate = false;
-		this.state = { fZero: 0, fOne: 0 };
+		this.state = { fZero: 0, fOne: 0, results: [] };
 	}
 	handleInputOne(e) {
 		this.setState({ fZero: parseInt(e.target.value, 10) });
@@ -13,9 +13,7 @@ export default class Fibonacci extends React.Component {
 		this.setState({ fOne: parseInt(e.target.value, 10) });
 	}
 	onClick() {
-		const params = {
-			values: [this.state.fZero, this.state.fOne]
-		};
+		const params = { fZero: this.state.fZero, fOne: this.state.fOne };
 		console.log([this.state.fZero, this.state.fOne])
 		$.ajax({
 			type: 'GET',
@@ -23,6 +21,7 @@ export default class Fibonacci extends React.Component {
 			url: '/api/',
 			success: results => {
 				console.log(results);
+				this.setState({ results });
 			}
 		})
 	}
@@ -33,6 +32,9 @@ export default class Fibonacci extends React.Component {
 			<span>
 				<button type='button' onClick={(e) => this.onClick()}>Calculate Fibonacci Sequence</button>
 			</span>
+			{this.state.results.map(result =>
+				<div><span>{ result }</span></div>
+			)}
 		</div>
 	}
 };
