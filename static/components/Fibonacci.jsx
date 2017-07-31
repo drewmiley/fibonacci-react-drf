97e2222373e425863b989df5c7cf9f9bc86c7c02
Drew Miley <drewmiley92@gmail.com>
2017-07-31 16:54:27 +0100

    Fixes ESLint errors

--- static/components/Fibonacci.jsx
+++ static/components/Fibonacci.jsx
@@ -21,18 +21,20 @@ export default class Fibonacci extends React.Component {
 			success: results => {
 				this.setState({ results });
 			}
-		})
+		});
 	}
 	render() {
-		return <div>
-			<input type='text' value={this.state.input} onChange={(e) => {this.handleInputOne(e)}} placeholder='F0'/>
-			<input type='text' value={this.state.input} onChange={(e) => {this.handleInputTwo(e)}} placeholder='F1'/>
-			<span>
-				<button type='button' onClick={(e) => this.onClick()}>Calculate Fibonacci Sequence</button>
-			</span>
-			{this.state.results.map(result =>
-				<div><span>{ result }</span></div>
-			)}
-		</div>
+		return (
+			<div>
+				<input type='text' value={this.state.input} onChange={e => { this.handleInputOne(e); }} placeholder='F(0)'/>
+				<input type='text' value={this.state.input} onChange={e => { this.handleInputTwo(e); }} placeholder='F(1)'/>
+				<span>
+					<button type='button' onClick={() => this.onClick()}>Calculate Fibonacci Sequence</button>
+				</span>
+				{this.state.results.map((result, i) =>
+					<div key={i}><span>{ result }</span></div>
+				)}
+			</div>
+		);
 	}
-};
+}
